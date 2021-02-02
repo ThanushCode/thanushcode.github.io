6f880f5d9132d4be2cf58fb42dd91eec84c08cda
Thanushcode <addtopay.com@gmail.com>
2021-02-02 23:18:04 +0100

    pi

--- js/poundsTokilograms3.js
+++ js/poundsTokilograms3.js
@@ -1,7 +1,7 @@
 function multiplyBy() {
     let x = parseFloat(document.getElementById("number1").value); //Input
    
-    let multiplyBy = x * 2.2046; // Processing
+    let multiplyBy = x * 0.45359237; // Processing
     document.getElementById("output").innerHTML = 
      x + " pounds equals " + multiplyBy + " kilograms"; // Output
   }
