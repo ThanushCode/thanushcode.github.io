c8ad3bec9123e6da88dc873ccb80257d9f58e670
Thanushcode <addtopay.com@gmail.com>
2021-02-04 01:31:08 +0100

    p

--- js/poundsToKilograms.js
+++ js/poundsToKilograms.js
@@ -1,7 +1,7 @@
 function poundsToKilograms() {
       let text = document.getElementById("pounds").value; //Input
      
-      let pounds = parseFloat (text) // Processing
+      let pounds = parseFloat (text); // Processing
       let kilograms = pounds * 0.45359237; 
       document.getElementById("output").textContent=  //output
       " pounds equals to " + `${kilograms.toFixed (1)}` + " kilograms " ;  
