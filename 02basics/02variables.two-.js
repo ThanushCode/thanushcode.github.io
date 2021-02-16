3b630636af78996b58cead06805f2bb5ec33b17d
Thanushcode <addtopay.com@gmail.com>
2021-02-16 14:53:51 +0100

    p

---
+++ 02basics/02variables.two-.js
@@ -0,0 +1,27 @@
+const uid = "abc123";
+
+var fullName = "Thansuh Ganesh";
+var email   = "thanushpen@gmail.com";
+var password = "123456";
+var confirmPassword ="123456";
+var courseCount= 0;
+var isLoggedInFromGoogle = false;
+
+
+//alert = this to pop up box which come in the website
+//if you want to get input from user something similer to alert which is prompt
+
+//fullname = prompt("Enter Your Name") // you assign promt to varable above full name but have to attach this file to website
+
+console.log(uid);
+console.log("Full Name Is: " + fullName);
+console.log(email);
+console.log(confirmPassword);
+
+console.log (`
+with unique id : ${uid}
+user is :  ${fullName}
+and his email is : ${email}
+and uses the password :${password}
+
+`)
