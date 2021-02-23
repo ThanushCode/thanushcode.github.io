59a4de5ba041ab34047d56e66d6d7a98c901f262
Thanushcode <addtopay.com@gmail.com>
2021-02-23 01:49:09 +0100

    p

--- js/forLoop.js
+++ js/forLoop.js
@@ -1,5 +1,11 @@
 //for (let count = 0; count < 5; count++) console.log(" number:" + count);
 // multiple for loop declaration
-for (let i = 0, j = 5; i < 5; i++, j--) {
-  console.log(i + "and" + j);
+//for (let i = 0, j = 5; i < 5; i++, j--) {
+// console.log(i + "and" + j);
+//}
+
+let i = 1;
+while (i <= 3) {
+  alert("leaf");
+  i++;
 }
